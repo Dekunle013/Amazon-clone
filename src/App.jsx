a0919ec1e2e71bd1d4f19dd170cae0f2e49b285f
Dekunle013 <Dekunle013@gmail.com>
2024-08-11 10:41:51 +0100

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+// import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      {/* <Nav /> */}
+      <Footer />
     </>
   )
 }
